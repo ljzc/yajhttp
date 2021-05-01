6e1e8884fa84dc2c02142f0284a0b9c79232423b
ljzc <1362288711@qq.com>
2021-05-01 09:26:35 +0800

    Add the implementation of option -u in UserCommandHandler

--- src/main/java/nju/yajhttp/client/command_handler_pipeline/handlerimp/UserCommandHandler.java
+++ src/main/java/nju/yajhttp/client/command_handler_pipeline/handlerimp/UserCommandHandler.java
@@ -2,9 +2,11 @@ package nju.yajhttp.client.command_handler_pipeline.handlerimp;
 
 import nju.yajhttp.client.command_handler_pipeline.ConstructRequestCommandHandler;
 import nju.yajhttp.client.command_handler_pipeline.Priority;
+import nju.yajhttp.message.Header;
 import nju.yajhttp.message.Request;
 
-import java.util.Arrays;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 public class UserCommandHandler extends ConstructRequestCommandHandler {
     public UserCommandHandler() {
@@ -14,7 +16,8 @@ public class UserCommandHandler extends ConstructRequestCommandHandler {
     @Override
     public Request handle(String[] args, Request request) {
         //todo
-
-        return null;
+        String nameAndPsw = args[args.length - 1]; //有多个的时候取最后一个参数作为最终结果
+        request.header(new Header("Authorization", "Basic " + Base64.getEncoder().encodeToString(nameAndPsw.getBytes(StandardCharsets.US_ASCII))));
+        return request;
     }
 }
